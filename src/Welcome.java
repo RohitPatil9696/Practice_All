76a822def66bf59ded226bc070f5a572c05fa709
RohitPatil9696 <patilrj741998@gmail.com>
2023-09-15 11:24:01 +0530

    Changes

--- src/Welcome.java
+++ src/Welcome.java
@@ -3,9 +3,11 @@ public class Welcome {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-      System.out.println("Welcome everyone");
-      System.out.println("Good Morning All");
+      System.out.println("Welcome everyone to all");
+      System.out.println("Good Afternoon to  All");
       System.out.println("Jay Siyaram");
+      System.out.println("Ram Ram bhai Sarrei ne");
+      
 	}
 
 }
